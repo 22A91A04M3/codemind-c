6a21a5e3a561f874040d895cb30d4c9f1843bea5
22A91A04M3 <119720475+22A91A04M3@users.noreply.github.com>
2022-12-18 18:46:29 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%3==0)     {         printf("Qualify");     }     else     {         printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%3==0)
+    {
+        printf("Qualify");
+    }
+    else
+    {
+        printf("Not Qualify");
+    }
+}
